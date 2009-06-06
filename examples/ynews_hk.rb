d2ef63fb2091cec7aefd8a118791c805d3b16158
siuying <siu.ying@gmail.com>
2009-06-06 17:00:13 +0800

    revise the example, set fetch limit to 2 for faster execution

--- examples/ynews_hk.rb
+++ examples/ynews_hk.rb
@@ -3,8 +3,8 @@ require "#{File.dirname(__FILE__)}/extractors/yahoo_news_hong_kong_extractor"
 
 # fetch news from Yahoo! News HK, at most fetch 20 pages, wait 2 seconds before each try
 feed = Fullfeed::Fullfeed.new("http://hk.news.yahoo.com/rss/hongkong/rss.xml",
-        :limit => 20,
-        :wait => 2)
+        :limit => 2,
+        :wait => 1)
 result = feed.fetch
 puts result
 
