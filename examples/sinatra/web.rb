1042c3975ec974d97adc62d0bf68cd7a989a1c88
siuying <siu.ying@gmail.com>
2009-06-06 19:47:10 +0800

    correct comment

--- examples/sinatra/web.rb
+++ examples/sinatra/web.rb
@@ -9,7 +9,7 @@ require 'sinatra'
 require "#{File.dirname(__FILE__)}/../../lib/fullfeed"
 require "#{File.dirname(__FILE__)}/../extractors/yahoo_news_hong_kong_extractor"
 
-# fetch news from Yahoo! News HK, at most fetch 20 pages, wait 2 seconds before each try
+# Create Yahoo! News HK full-text feed
 feed = Fullfeed::Feed.new("http://hk.news.yahoo.com/rss/hongkong/rss.xml",
         :limit => 20,
         :wait => 1,
