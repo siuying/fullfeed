6c814c2ec91c2bcd1b7f7d82a9e2d5f6b2462387
siuying <siu.ying@gmail.com>
2009-06-06 23:18:10 +0800

    fetch fewer items

--- examples/ynews_hk.rb
+++ examples/ynews_hk.rb
@@ -6,9 +6,9 @@ require "#{File.dirname(__FILE__)}/../lib/fullfeed"
 require "#{File.dirname(__FILE__)}/extractors/yahoo_news_hong_kong_extractor"
 
 # create full text RSS feed from Yahoo! News HK
-# At most fetch 20 pages, wait 1 seconds before each try
+# At most fetch 5 pages, wait 1 seconds before each try
 feed = Fullfeed::Feed.new("http://hk.news.yahoo.com/rss/hongkong/rss.xml",
-        :limit => 20,
+        :limit => 5,
         :wait => 1)
 result = feed.fetch
 puts result
